7bc8e00735d11e95e1366ffcedd2228b43593ccc
Petr Plavjanik <petr.plavjanik@broadcom.com>
2019-05-13 23:23:17 +0200

    Remove deployment script

--- script/dev/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-REPO=$(dirname "$0")/../..
-TARGET_DIR=/a/plape03/zowesample
-
-zowe files upload file-to-uss --binary ${REPO}/build/libs/zowe-apiservice-0.0.1-SNAPSHOT.jar ${TARGET_DIR}/zowe-apiservice-0.0.1-SNAPSHOT.jar
-zowe files upload file-to-uss --binary ${REPO}/config/local/application.yml ${TARGET_DIR}/application.yml
-zowe files upload file-to-uss --binary ${REPO}/config/local/keystore.p12 ${TARGET_DIR}/keystore.p12
-zowe files upload file-to-uss --binary ${REPO}/config/local/truststore.p12 ${TARGET_DIR}/truststore.p12
